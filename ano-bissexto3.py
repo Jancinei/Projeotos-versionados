e843474aa89134cc6beae359674c3bff6fbd970e
Jancinei Apóstolo Evangelista <pr.jancinei@hotmail.com>
2025-06-23 16:54:30 -0300

    Ano atual
    Importação da biblioteca datetime

--- ano-bissexto3.py
+++ ano-bissexto3.py
@@ -1,4 +1,7 @@
+import datetime
 ano = int(input('Quer saber se um determinado ano é bissexto? digite o ano aqui!  '))
+if ano == 0:
+    ano = datetime.date.today() .year
 if ano % 2 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print (f'O ano {ano} é bissexto')
 else:
