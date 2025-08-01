1e039690ed418e85be7cdceaf5f7bbcdf11cb608
Jancinei Apóstolo Evangelista <pr.jancinei@hotmail.com>
2025-07-31 21:59:15 -0300

    Exercício38
    Do Mód. 2

---
+++ maior_número.py
@@ -0,0 +1,8 @@
+num1 = eval(input('Digite um número qualquer  '))
+num2 = eval(input('Digite outro número   '))
+if num1 > num2:
+    print (f'{num1} é maior que {num2}')
+elif num1 == num2:
+    print (' Você digitou nuúmeros iguais')
+else:
+    print (f'{num2} é maior que {num1}')
