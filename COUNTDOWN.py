896d76c1cc00df41834b18aa57feb88193e71084
Jancinei Apóstolo Evangelista <pr.jancinei@hotmail.com>
2025-08-18 08:40:17 -0300

    Update COUNTDOWN.py

--- COUNTDOWN.py
+++ COUNTDOWN.py
@@ -1,7 +1,7 @@
 from random import randint
 from time import sleep
 countdown = int(input('Contagem regressiva, insira os minutos desejados  '))
-for c in range(countdown, 0,  -1):
+for c in range(countdown, 0 -1,  -1):
     print(c)
     sleep(1)
-print('START')
+print(f'\033[1;31mFELIZ ANO NOVO!\033[m')
