e942f74f3622cedf6d68ec02337796802baa5f42
Jancinei Apóstolo Evangelista <pr.jancinei@hotmail.com>
2025-08-18 08:15:43 -0300

    Laço

---
+++ laço.py
@@ -0,0 +1,8 @@
+from random import randint
+from time import sleep
+accont = int(input('Quantos segundos você quer contar?  '))
+for c in range(0,accont+1):
+    print(c)
+    sleep(1)
+sleep(1)
+print('END!')
