d22adc5453a22cfd7c460820b82b02e0df41a719
Jancinei Apóstolo Evangelista <pr.jancinei@hotmail.com>
2025-09-23 18:40:38 -0300

    decisão2

---
+++ dados_pessoais.py
@@ -0,0 +1,5 @@
+for dados in range(1, 3):
+    nome= input(f'Digite o nome do presidente  ')
+    nome1= input(f'Digite o nome do vice presidente  ')
+    nome2= input(f'Digite o nome do tesoureiro  ')
+    nome3= input(f'Digite o nome do vice secretário  ')
