9829ceec2f38f98443c8471fc6037e0895f0a476
Jancinei Apóstolo Evangelista <pr.jancinei@hotmail.com>
2026-06-22 21:47:05 -0300

    gerador de PA

---
+++ geradordePA.py
@@ -0,0 +1,10 @@
+import time
+primeiro = int(input('Digite o primeiro termo   '))
+razao = int(input('Digite a razão  '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print('{} -'.format(termo),  end='')
+    termo = termo + razao
+    cont +=1
+   
